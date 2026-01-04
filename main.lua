3b8da8831e4658a052cbbd17088dcfaf3d0a021c
PrimeXploit <forgivetomyct@gmail.com>
2026-01-05 01:11:06 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,22 @@
-local SettingsPlace = {
-    ["71793674075007"] = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/PrimeXploit/script/refs/heads/main/script/kanom-tokyo.lua"))()'
+local SettingsUniverse = {
+    ["71793674075007"] = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/PrimeXploit/script/refs/heads/main/script/kanom-tokyo.lua"))()',
+	["18172550962"] = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/PrimeXploit/script/refs/heads/main/script/pixel-blade.lua"))()',
 }
 
-local PlaceId = tostring(game.PlaceId)
+local SettingsPlace = {
+
+}
+
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
-local Character = Player.Character or Player.CharacterAdded:Wait()
-local Humanoid = Character:WaitForChild("Humanoid")
 
-if SettingsPlace[PlaceId] then
-    loadstring(SettingsPlace[PlaceId])()
+local placeId = tostring(game.PlaceId)
+local universeId = tostring(game.GameId)
+
+local scriptToRun = SettingsPlace[placeId] or SettingsUniverse[universeId]
+
+if scriptToRun then
+	loadstring(scriptToRun)()
 else
-    Player:Kick("This script isn’t supported yet")
+	Player:Kick("This script isn’t supported yet")
 end
